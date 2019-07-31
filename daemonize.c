a2e157a90d48b339877628558c4d6e4ee7fe0eb5
Pascal Werkl <pasce@users.noreply.github.com>
2019-07-31 11:46:33 +0200

    Create daemonize.c

---
+++ daemonize.c
@@ -0,0 +1,87 @@
+/*
+ * daemonize.c
+ * This example daemonizes a process, writes a few log messages,
+ * sleeps 20 seconds and terminates afterwards.
+ * This is an answer to the stackoverflow question:
+ * https://stackoverflow.com/questions/17954432/creating-a-daemon-in-linux/17955149#17955149
+ * Fork this code: https://github.com/pasce/daemon-skeleton-linux-c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+
+static void skeleton_daemon()
+{
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    /* Fork off for the second time*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* Set new file permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close (x);
+    }
+
+    /* Open the log file */
+    openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
+}
+
+int main()
+{
+    skeleton_daemon();
+
+    while (1)
+    {
+        //TODO: Insert daemon code here.
+        syslog (LOG_NOTICE, "First daemon started.");
+        sleep (20);
+        break;
+    }
+
+    syslog (LOG_NOTICE, "First daemon terminated.");
+    closelog();
+
+    return EXIT_SUCCESS;
+}
